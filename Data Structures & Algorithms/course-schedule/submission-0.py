b820eff17557bc13170f033faad9c9888dac4669
Anikesh-99 <b.anikesh@gmail.com>
2026-07-15 22:58:25 -0700

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        startPoints = set([i for i in range(numCourses)])
+        mapping = {}
+        revMap = {}
+        for x, y in prerequisites:
+            if x not in mapping:
+                mapping[x] = [y]
+                startPoints.remove(x)
+            else:
+                mapping[x].append(y)
+            if y not in revMap:
+                revMap[y] = [x]
+            else:
+                revMap[y].append(x)
+        # print(startPoints)
+        if not startPoints: return False
+        startPoints = list(startPoints)
+        visited = set()
+        while startPoints:
+            curr = startPoints.pop(0)
+            if curr in visited: continue
+            visited.add(curr)
+            dep = revMap[curr] if curr in revMap else []
+            for d in dep:
+                mapping[d].remove(curr)
+                if not mapping[d]:
+                    startPoints.append(d)
+        return len(visited) == numCourses
+            
